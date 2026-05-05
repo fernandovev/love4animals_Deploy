8178da4cc5a15df10f3a74b77a0fd9316cc73ec9
AlessanderMorales <alessmoralesmo@gmail.com>
2026-05-05 02:02:38 -0400

    Feat

--- Love4AnimalsApi/Program.cs
+++ Love4AnimalsApi/Program.cs
@@ -7,33 +7,26 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Controllers + OpenAPI
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
-// EF Core para Post y Comment
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(connectionString));
 
-// Users en memoria
 builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddSingleton<IUserRepository, UserRepository>();
 
-// Campaigns en memoria
 builder.Services.AddScoped<ICampaignService, CampaignService>();
 builder.Services.AddSingleton<ICampaignRepository, CampaignRepository>();
 
-// Posts con EF Core
 builder.Services.AddScoped<IPostService, PostService>();
 builder.Services.AddScoped<IPostRepository, PostRepository>();
 
-// Comments con EF Core
 builder.Services.AddScoped<ICommentService, CommentService>();
 builder.Services.AddScoped<ICommentRepository, CommentRepository>();
 
-// Donations en memoria
 builder.Services.AddScoped<IDonationService, DonationService>();
 builder.Services.AddSingleton<IDonationRepository, DonationRepository>();
 
